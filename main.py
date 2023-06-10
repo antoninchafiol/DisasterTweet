2eb7944c390032887e3540c533dd92b5fee4d2d4
antoninchafiol <antonin.chafiol@gmail.com>
2023-06-11 01:17:22 +0200

    Preparation of Vectorization

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ import torch.optim as optim
 import pandas as pd
 import numpy as np
 import re
+import seaborn as sns
 
 import matplotlib.pyplot as plt
 
+from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
+from torchtext.vocab import GloVe
+from gensim import 
+
 from torch.utils.data import Dataset
 
-
-
-
-if __name__ == '__main__':
-
-    df = pd.read_csv("dataset/train.csv")
+def EDA(df):
     print("-----------------------[Information]-------------------------") 
     print(df.info()) 
     print("-----------------------[Top of Data]-------------------------")
@@ -24,3 +24,49 @@ if __name__ == '__main__':
     print("-----------------------[NaN Elements]-------------------------")
     print(df.isna().sum())
     print("--------------------------------------------------------------")
+
+
+    df.isnull().sum().plot.bar()
+    plt.show()
+    df['t_mean'] = df.groupby('keyword')['target'].transform('mean')
+    fig = plt.figure(figsize=(8, 72), dpi=100)
+
+    sns.countplot(y=df.sort_values(by='t_mean', ascending=False)['keyword'],
+                hue=df.sort_values(by='t_mean', ascending=False)['target'])
+
+    plt.tick_params(axis='x', labelsize=15)
+    plt.tick_params(axis='y', labelsize=12)
+    plt.legend(loc=1)
+    plt.title('Target Distribution in Keywords')
+    plt.show()
+
+    df.drop(columns=['t_mean'], inplace=True)
+
+    #Still need things:
+    
+    # word_count number of words in text
+    # unique_word_count number of unique words in text
+    # stop_word_count number of stop words in text
+    # url_count number of urls in text
+    # mean_word_length average character count in words
+    # char_count number of characters in text
+    # punctuation_count number of punctuations in text
+    # hashtag_count number of hashtags (#) in text
+    # mention_count number of mentions (@) in text
+
+def cleaning():
+    re.sub(r'()', )
+    return 0 
+
+def embedding():
+    # CountVector
+    # Tfid
+    # GloVe
+    # word2Vec
+    return 0 
+
+if __name__ == '__main__':
+    df = pd.read_csv("dataset/train.csv")
+    # print(df["text"][0:5])
+    embedding()
+    # EDA(df)
