33ae4d0fd2bce45861ff9d2ae047cdd47bfd701f
antoninchafiol <antonin.chafiol@gmail.com>
2023-08-10 00:10:50 +0200

    [UPDATE] Start positionalEncoding

--- tranformer_classes.py
+++ tranformer_classes.py
@@ -2,7 +2,20 @@
 import torch
 
 class Embedder(torch.nn.Module):
+    '''
+    The Embedding layer in an allocated class
+    '''
     def __init__(self, params, d_model) -> None:
+        '''
+        Initialize and freeze the embedding layer
+
+        Parameters
+        ----------
+        params --> ['vocab_size']: dict --> int
+            All params related to model --> Size of the vocabulary
+        d_model: int
+            Dimension of the model
+        '''
         super().__init__()
         self.embed = torch.nn.Embedding(params['vocab_size'], d_model)
         self.embed.weight.data.copy_(d_model)
@@ -10,3 +23,11 @@ class Embedder(torch.nn.Module):
     def forward(self, x):
         return self.embed(x)
     
+class PositionalEncoder(torch.nn.Module):
+    def __init__(self, max_seq_length, d_model):
+        super().__init__()
+        self.d_model = d_model
+        self.pe = torch.zeros(max_seq_length, d_model)
+        
+    def forwward(self, x):
+        return x
