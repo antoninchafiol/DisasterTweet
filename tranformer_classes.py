beddb2f9fe69cf559a0544e702c370a3b6830400
antoninchafiol <antonin.chafiol@gmail.com>
2023-08-13 16:29:54 +0200

    [ADD] SeqToSeq Transformer class

--- tranformer_classes.py
+++ tranformer_classes.py
@@ -404,7 +404,20 @@ class Decoder(nn.Module):
         x = self.norm
         return x    
 
-
+class SeqToSeqTransformer(nn.Module):
+    '''
+    Create a sequence to sequence tranformer network.
+    '''
+    def __init__(self, src_vocab, trg_vocab, d_model, N, heads):
+        super().__init__()
+        self.encoder = Encoder(src_vocab, d_model, N, heads)
+        self.decoder = Decoder(trg_vocab, d_model, N, heads)
+        self.out = nn.Linear(d_model, trg_vocab)
+    def forward(self, src, trg, src_mask, trg_mask):
+        e_outputs = self.encoder(src, src_mask)
+        d_output = self.decoder(trg, e_outputs, src_mask, trg_mask)
+        output = self.out(d_output)
+        return output
 
 
 
